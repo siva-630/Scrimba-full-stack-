634f843648dbf8ace5680cc4cc8538e81d806c53
siva-630 <siva630029@gmail.com>
2026-04-24 13:14:25 +0530

    day1

---
+++ javascript/first.js
@@ -0,0 +1,8 @@
+console.log(myage);
+
+let myage = 10;
+
+
+// 🤔ReferenceError: Cannot access 'myage' before initialization
+// -----------------------------------------------------------------------------------------------------------------------------
+
